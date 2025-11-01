bda76fb647e77e95276a38fca38777746b22a936
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2025-11-01 20:18:13 +0000

    added subqueries

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,16 @@
+SELECT * 
+FROM property p
+WHERE (
+  SELECT AVG(rating)
+  FROM review r
+  WHERE r.property_id = p.property_id
+) > 4
+
+
+SELECT *
+FROM user u
+WHERE (
+  SELECT COUNT(*)
+  FROM booking b
+  WHERE b.user_id = u.user_id
+) > 3
