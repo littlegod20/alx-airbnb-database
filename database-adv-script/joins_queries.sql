ff6a7c6fa5dc60bbc66c0c2786062295d8173bb5
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2025-11-01 19:20:58 +0000

    added database-advs-script

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,17 @@
+-- INNER JOIN
+SELECT *
+FROM booking as b
+INNER JOIN user as u
+ON b.user_id = u.user_id;
+
+-- LEFT JOIN
+SELECT *
+FROM property p
+LEFT JOIN review r
+ON p.property_id = r.property_id
+
+--FULL OUTER JOIN
+SELECT * 
+FROM user u
+FULL OUTER JOIN booking b
+ON u.user_id = b.user_id
